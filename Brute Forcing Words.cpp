86a6b43c525cf40efb33795bb6a2c31b717de2dd
bitaxura <142764261+bitaxura@users.noreply.github.com>
2024-03-26 12:31:07 -0400

    Update Brute Forcing Words.cpp

--- Brute Forcing Words.cpp
+++ Brute Forcing Words.cpp
@@ -5,26 +5,31 @@
 using namespace std;
 
 int main(){
-    char letters[27] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '};
-    string word = "HELLO WORLD";
+    string word = "HELLO WORLD"; //You can change the word here, it must be in all uppercase, if you want to make it all lowercase, change the for loop below
     vector<char> words;
     int i = 0;
 
     for(char c : word){
-        
-        for(int i = 0; i<27;  i++){
+        for(int i = 65; i<=90;  i++){ //For UPPERCASE: i = 65 & i<=90, For lowercase i = 97 & i <=122
             for(char d : words){
             cout<<d;
-        }
+            }
         
-            this_thread::sleep_for(chrono::milliseconds(30)); //Adjusts the delay added to change display speed
-            cout<<letters[i]/*<<"\r"*/<<endl; //Uncomment and remove endl to update text on the same line
-            cout.flush();
+            this_thread::sleep_for(chrono::milliseconds(20)); //Adjusts the delay added to change display speed
+            cout<<char(i)<</*" \r"*/endl; //Uncomment and remove endl to update text on the same line  
 
-            if(letters[i] == c){
-                words.push_back(letters[i]);
+            if(char(i) == c){
+                words.push_back(char(i));
                 break;
             }
+            else if(c == ' '){
+                words.push_back(' ');
+                break;
+            }
+        }
+        if(words.size() == 0){
+            cout<<"Invalid Input"<<endl;
+            return 0;
         }
     }
     cout<<endl;
