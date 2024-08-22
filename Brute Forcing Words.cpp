f5ba7dbc94ca9e5373f7aafe1483181c23faf253
Daksh Jain <142764261+bitaxura@users.noreply.github.com>
2024-08-22 12:30:41 -0400

    Update Brute Forcing Words.cpp

--- Brute Forcing Words.cpp
+++ Brute Forcing Words.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 
 int main(){
-    string word = "HELLO WORLD"; //Change the word here, by default it must be all UPPERCASE, to change to all lowercase, edit the for loop below
+    string word = "HELLO WORLD"; //Change the Word Here, must be all lowercase or uppercase, change the for loop below accordingly
     vector<char> words;
     int i = 0;
 
     for(char c : word){
         for(int i = 65; i<=90;  i++){ //For UPPERCASE: i = 65 & i<=90, For lowercase i = 97 & i <=122
             for(char d : words){
-            cout<<d;
+                cout<<d;
             }
         
-            this_thread::sleep_for(chrono::milliseconds(20)); //Adjusts the delay added to change display speed
-            cout<<char(i)<</*" \r"*/endl; //Uncomment and remove endl to update text on the same line  
+            this_thread::sleep_for(chrono::milliseconds(30)); //Adjusts the delay added to change display speed
+            cout<<char(i)<</*" \r";*/endl; //Uncomment \r and comment endl to update text on the same line  
 
             if(char(i) == c){
                 words.push_back(char(i));
@@ -26,10 +26,6 @@ int main(){
                 words.push_back(char(32));
                 break;
             }
-        }
-        if(words.size() == 0){
-            cout<<"Invalid Input"<<endl;
-            return 0;
         }
     }
     cout<<endl;
